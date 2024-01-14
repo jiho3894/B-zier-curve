c1c3cb3a0d143125d767e2fdad0a0f6b7825c248
Charley <rlawlgh3894@naver.com>
2024-01-14 14:33:37 +0900

    default setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,19 @@
 import TrainingData from '@/features/components/TrainingData';
 import styled from '@emotion/styled';
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
   const [showData, setShowData] = useState<boolean>(false);
-  const [inputVisible, setInputVisible] = useState(true);
-  const inputRef = useRef(null);
 
   const showSumData = () => {
     setShowData((prev) => !prev);
-    setInputVisible(true);
   };
-
-  const handleInputFocus = () => {
-    setInputVisible(false); // input 숨김
-  };
-
-  // Swift에서 호출할 함수
-  useEffect(() => {
-    // window.showInput = () => {
-    //   setInputVisible(true); // input 보임
-    // };
-  }, []);
   return (
     <>
       <Container>
         <div onClick={showSumData}>본인 등급의 평균 {showData ? '가리기' : '보기'} </div>
         <TrainingData />
       </Container>
-      <Textarea ref={inputRef} style={{ opacity: inputVisible ? 1 : 0 }} onFocus={handleInputFocus} />
     </>
   );
 }
